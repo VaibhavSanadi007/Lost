460313492b3e60a8a5837b8e8bd60dbdf37267de
Vaibhav Sanadi <157675679+VaibhavSanadi007@users.noreply.github.com>
2025-10-11 03:16:38 +0530

    Update ViewFollowers.tsx

--- front/src/components/ViewFollowers.tsx
+++ front/src/components/ViewFollowers.tsx
@@ -1,18 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import type { ObjType } from "../store/userSlice";
-import type { Dispatch, FC, SetStateAction } from "react";
-
-type property = {
-  setopenFollowers : Dispatch<SetStateAction<boolean>>;
-  followData : ObjType[];
-}
-
-const ViewFollowers:FC<property> = ({setopenFollowers , followData}) => {
-    const navigate = useNavigate();
+const ViewFollowers= () => {
+  
   
   return (
-   <>
-   </>
+   <div>
+     hello
+   </div>
   )
 }
 export default ViewFollowers
