e03a9ea1921ec30ddeae1a6cfdbe30630711df28
vaibhav <vaibhavbhausanadi0007@gmail.com>
2025-11-11 10:16:19 +0530

    once again ui fixed

--- front/src/pages/MobileMenu.tsx
+++ front/src/pages/MobileMenu.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import type { RootState } from "../store/reduxStore";
 
 const MobileMenu = () => {
+  const userData = useSelector((item: RootState) => item.user);
   const navigate = useNavigate();
   const [menuFlag,setMenuFlag] = useState<boolean>(false);
   return (
@@ -10,9 +13,9 @@ const MobileMenu = () => {
      {menuFlag && <div className="flex flex-col gap-3 w-40 py-2 md:w-60 md:py-5" onChange={(e)=>{e.stopPropagation()}}> 
         <nav className="text-end px-3 md:px-5 md:text-2xl" onClick={()=>setMenuFlag(false)}>X</nav>
         <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=> navigate(`/home`)} >feed</button>
-        {/* <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/messagelist/${userData._id}`)}>friends</button> */}
+        <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/messagelist/${userData._id}`)}>friends</button>
         <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/explore`)}>explore</button>
-        {/* <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/profile/${userData._id}`)}>profile</button> */}
+        <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/profile/${userData._id}`)}>profile</button>
         <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/setting`)}>settings</button>
         <button className="active:scale-90 duration-200 md:text-2xl" onClick={()=>navigate(`/createpost`)}>create post</button>
       </div>}
